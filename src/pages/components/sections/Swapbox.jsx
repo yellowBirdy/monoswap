fec0acf27d2f1b7865379ac5ffd5650179f7d564
Mieszko Manijak <mieszko@grit42.com>
2020-11-08 00:14:26 +0100

    dirty getAmoutOut  in the Sandbox (fcl)

--- src/pages/components/sections/Swapbox.jsx
+++ src/pages/components/sections/Swapbox.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Tab} from 'semantic-ui-react'
 
 
-
-export default ({}) => {
-    return (
-        <div>
-            <form id="swapbox-form" onSubmit={e=>{e.preventDefault(); alert(e.target)}} style={{margin: "auto", width:"50%"}} >
+const panes = [
+    {   
+        menuItem: "Buy Bitroot",
+        render: () =>            
+            <form id="swapbox-form-bitroot" onSubmit={e=>{e.preventDefault(); alert(e.target)}} style={{margin: "auto", width:"50%"}} >
                 <label style={{display:"block", width: "90%", margin:"auto", textAlign:"center"}}>Flow amount:
                 <input type="numeric"  style={{display:"block", width:"100%"}}></input>
                 </label>
@@ -13,7 +14,24 @@ export default ({}) => {
                 <input type="numeric"  style={{display:"block", width:"100%"}}></input>
                 </label>
                 <input type="submit" value="Swap" style={{display:"block", width: "90%", margin:"auto"}} />
-            </form>
-        </div>
+            </form>,
+    },{
+        menuItem: "Buy Flow",
+        render: () =>            
+            <form id="swapbox-form-flow" onSubmit={e=>{e.preventDefault(); alert(e.target)}} style={{margin: "auto", width:"50%"}} >
+                <label style={{display:"block", width: "90%", margin:"auto", textAlign:"center"}}>Bitroot amount:
+                <input type="numeric"  style={{display:"block", width:"100%"}}></input>
+                </label>
+                <label style={{display:"block", width: "90%", margin:"auto", textAlign:"center"}}>Flow amount:
+                <input type="numeric"  style={{display:"block", width:"100%"}}></input>
+                </label>
+                <input type="submit" value="Swap" style={{display:"block", width: "90%", margin:"auto"}} />
+            </form>,
+    }
+]
+ 
+export default ({}) => {
+    return (
+        <Tab panes={panes} />
     )
 }
